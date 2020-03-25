b90b0a7143be8aed2e4c6f8c6a43d1ca67bf0ea2
yfzjay <yuanziang1997@qq.com>
2020-03-25 16:18:03 +0800

    20200325

--- src/main/java/LeetCode/array/L485.java
+++ src/main/java/LeetCode/array/L485.java
@@ -1,4 +1,17 @@
 package LeetCode.array;
 
 public class L485 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int tmp=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                tmp+=1;
+                max=Math.max(tmp,max);
+            }else{
+                tmp=0;
+            }
+        }
+        return max;
+    }
 }
